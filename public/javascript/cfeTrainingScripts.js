a04bef23d17a6809cc281d1d360873c98466607a
TylerKingsley <tyler@kingsleyrobins.com>
2023-10-26 12:59:13 +0200

    Refactored the JS

--- public/javascript/cfeTrainingScripts.js
+++ public/javascript/cfeTrainingScripts.js
@@ -1,58 +1,79 @@
 document.addEventListener("DOMContentLoaded", function() {
+    // Elements
     const videoElement = document.getElementById('cfeTrainingVideo');
     const continueButton = document.getElementById('videoCompletedButton');
     const closeModal = document.getElementById('closeModal');
     const modal = document.getElementById('customModal');
     const modalButton = document.getElementById('modalButton');
-    
-    videoElement.addEventListener('ended', function() {
+
+    // Event Listeners
+    videoElement.addEventListener('ended', videoEndedHandler);
+    closeModal.addEventListener('click', closeModalHandler);
+    modalButton.addEventListener('click', modalButtonHandler);
+    document.getElementById('cfeTrainingVideo').addEventListener('play', orientationCheck);
+    window.addEventListener("click", fullscreen, { once: true });
+    window.addEventListener('orientationchange', orientationChangeHandler);
+    adjustVideoSize();
+
+    // Handlers
+    function videoEndedHandler() {
         continueButton.style.display = 'block';  // Show the Continue button
+        adjustVideoSize();  // Adjust video size
         modal.style.display = 'block';  // Show the modal
-    });
+    }
 
-    closeModal.addEventListener('click', function() {
+    function closeModalHandler() {
         modal.style.display = 'none';  // Hide the modal when close button is clicked
-    });
+    }
 
-    modalButton.addEventListener('click', function() {
+    function modalButtonHandler() {
         modal.style.display = 'none';  // Hide the modal when modal's continue button is clicked
         // Add any further action you want to take after clicking the modal's continue button here.
-    });
+    }
+
+    function orientationCheck() {
+        if (window.innerHeight > window.innerWidth) {
+            this.pause();
+            alert('Please rotate your phone to landscape mode to watch the video.');
+        }
+    }
+
+    function orientationChangeHandler() {
+        if (window.innerWidth <= window.innerHeight) {
+            videoElement.pause();
+        }
+    }
+    // Fullscreen function  
+    function fullscreen() {
+        var element = document.documentElement;
+        if (element.requestFullscreen) {
+            element.requestFullscreen();
+        } else if (element.mozRequestFullScreen) {
+            element.mozRequestFullScreen();
+        } else if (element.webkitRequestFullscreen) {
+            element.webkitRequestFullscreen();
+        } else if (element.msRequestFullscreen) {
+            element.msRequestFullscreen();
+        }
+    }
+
+    // Adjust size for continue button
+    function adjustVideoSize() {
+        const video = document.getElementById('cfeTrainingVideo');
+        const button = document.getElementById('videoCompletedButton');
+    
+        // Check if both video and button elements exist
+        if (!video || !button) return;
+    
+        // Check if the button is displayed or not
+        const isButtonDisplayed = window.getComputedStyle(button).display !== 'none';
+    
+        if (isButtonDisplayed) {
+            video.style.maxHeight = '85vh';  // Adjusted for visible button
+        } else {
+            video.style.maxHeight = '98vh';  // Adjusted for hidden button
+        }
+    }
 });
 
-// Fullscreen function
-function fullscreen() {
-    var element = document.documentElement;
-    if (element.requestFullscreen) {
-        element.requestFullscreen();
-    } else if (element.mozRequestFullScreen) {
-        element.mozRequestFullScreen();
-    } else if (element.webkitRequestFullscreen) {
-        element.webkitRequestFullscreen();
-    } else if (element.msRequestFullscreen) {
-        element.msRequestFullscreen();
-    }
-}
 
-// Check for orientation and prevent video from playing in portrait mode
-document.getElementById('cfeTrainingVideo').addEventListener('play', function() {
-    if (window.innerHeight > window.innerWidth) {
-        this.pause();
-        alert('Please rotate your phone to landscape mode to watch the video.');
-    }
-});
-
-// Add fullscreen event listener
-window.addEventListener("click", fullscreen, { once: true });
-
-// Check for orientation change
-window.addEventListener('orientationchange', function() {
-    const videoElement = document.getElementById('cfeTrainingVideo');
-    if (window.innerWidth > window.innerHeight) {
-        // If the device is in landscape mode, play the video
-        videoElement.play();
-    }
-    else {
-        videoElement.pause();
-    }
-});
